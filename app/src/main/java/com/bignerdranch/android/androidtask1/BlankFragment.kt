9726afb6675092ba880c988661b22544e38d74eb
kndnba <dedmombed@gmail.com>
2022-12-08 11:47:42 +0600

    FAT-47439: added linear layout with 2 textviews, added fragment with edittext, textwatcher and toast

---
+++ app/src/main/java/com/bignerdranch/android/androidtask1/BlankFragment.kt
@@ -0,0 +1,58 @@
+package com.bignerdranch.android.androidtask1
+
+import android.os.Bundle
+import android.os.TokenWatcher
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.bundleOf
+
+import com.bignerdranch.android.androidtask1.databinding.FragmentBlankBinding
+
+
+class BlankFragment : Fragment() {
+        lateinit var editText: EditText
+        lateinit var textView: TextView
+        lateinit var v: View
+        lateinit var binding: FragmentBlankBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBlankBinding.inflate(layoutInflater)
+        v = inflater.inflate(R.layout.fragment_blank, container, false)
+        editText = v.findViewById(R.id.input_field)
+
+        binding.inputField.onTextChange{
+            showToast(it)
+        }
+        return binding.root
+    }
+
+
+    inline fun EditText.onTextChange(crossinline listener: (String) -> Unit) {
+        this.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //NO OP
+            }
+            override fun onTextChanged(charSequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                listener(charSequence.toString())
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                //NO OP
+            }
+        })
+    }
+    private fun showToast(text: String) {
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/com/bignerdranch/android/androidtask1/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/androidtask1/MainActivity.kt
@@ -2,10 +2,27 @@ package com.bignerdranch.android.androidtask1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.bignerdranch.android.androidtask1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+    lateinit var fragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        mBinding.button2.setOnClickListener {
+            mBinding.button2.visibility = View.GONE
+            mBinding.textView.visibility = View.GONE
+            mBinding.textView2.visibility = View.GONE
+            fragment = BlankFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.container, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
