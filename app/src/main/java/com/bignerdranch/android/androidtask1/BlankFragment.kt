fe8e91d05f1cb06f98179cc0d3131874eb6abd31
kndnba <dedmombed@gmail.com>
2022-12-14 18:25:34 +0600

    FAT-47439: some changes

--- app/src/main/java/com/bignerdranch/android/androidtask1/BlankFragment.kt
+++ app/src/main/java/com/bignerdranch/android/androidtask1/BlankFragment.kt
@@ -1,8 +1,10 @@
 package com.bignerdranch.android.androidtask1
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,21 +13,67 @@ import android.widget.EditText
 import android.widget.Toast
 import com.bignerdranch.android.androidtask1.databinding.FragmentBlankBinding
 
+private const val TAG = "BlankFragment"
 
 class BlankFragment : Fragment() {
     private lateinit var editText: EditText
     private lateinit var binding: FragmentBlankBinding
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach() called")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate() called")
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d(TAG, "onCreateView() called")
         binding = FragmentBlankBinding.inflate(layoutInflater)
         editText = binding.inputField
         binding.inputField.onTextChange {
             showToast(it)
         }
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "onDetach() called")
     }
 
     inline fun EditText.onTextChange(crossinline listener: (String) -> Unit) {
--- app/src/main/java/com/bignerdranch/android/androidtask1/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/androidtask1/MainActivity.kt
@@ -2,11 +2,14 @@ package com.bignerdranch.android.androidtask1
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.bignerdranch.android.androidtask1.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
@@ -33,6 +36,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toolbar.setOnClickListener{
             drawerLayout.open()
         }
+
     }
 
     override fun onStart() {
@@ -61,22 +65,34 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val id: Int = item.itemId
 
-        if (id == R.id.recyclerViewMenu){
+        fragment = Fragment()
+
+        when (item.itemId){
+            R.id.recyclerViewMenu -> {
             fragment = RecyclerViewFragment()
             commitFragment()
-        } else if (id == R.id.fragment_menu){
+            }
+            R.id.fragment_menu -> {
             fragment = BlankFragment()
             commitFragment()
+            }
+            R.id.task2element1 -> Toast.makeText(this, "Task 2 element 1", Toast.LENGTH_SHORT).show()
+            R.id.task2element2 -> Toast.makeText(this, "Task 2 element 2", Toast.LENGTH_SHORT).show()
+            R.id.task3element1 -> Toast.makeText(this, "Task 3 element 1", Toast.LENGTH_SHORT).show()
+            R.id.task3element2 -> Toast.makeText(this, "Task 3 element 2", Toast.LENGTH_SHORT).show()
+            R.id.task4element1 -> Toast.makeText(this, "Task 4 element 1", Toast.LENGTH_SHORT).show()
+            R.id.task4element2 -> Toast.makeText(this, "Task 4 element 2", Toast.LENGTH_SHORT).show()
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 
     private fun commitFragment(){
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.container, fragment)
-        fragmentTransaction.commit()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, fragment)
+            .replace(R.id.container, fragment)
+            .commit()
     }
+
 }
